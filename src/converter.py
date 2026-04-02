93c08028e7e662fee5e8e7805fe23fd0dd3d235e
Radoslaw Kaleba <radoslaw.kaleba@gmail.com>
2026-04-03 00:53:03 +0200

    Added functionality, if date of correct format in the original file exists, it skips it just like with empty rows

--- src/converter.py
+++ src/converter.py
@@ -2,9 +2,13 @@ import csv
 import datetime
 
 def convert_date(date_string):
-    date_object = datetime.datetime.strptime(date_string, "%B %d, %Y")
-    converted = date_object.strftime("%Y-%m-%d")
-    return converted
+    try:
+        date_object = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+        return date_string
+    except:
+        date_object = datetime.datetime.strptime(date_string, "%B %d, %Y")
+        converted = date_object.strftime("%Y-%m-%d")
+        return converted
 
 def process_file(input_file, output_file):
     with open(input_file, "r") as infile, open(output_file, "w") as outfile:
